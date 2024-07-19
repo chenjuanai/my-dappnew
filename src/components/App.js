02d7e69779d91d27103aaac54b2642761699cd2c
chenjuanai <chenjanai@hotmailcom>
2024-07-19 23:15:36 +0800

    Configured module-resolver and updated import paths

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
 const { ethers } = window.ethers.default;
-import WalletConnectButton from './WalletConnectButton.js';
-import SignatureModal from './SignatureModal.js';
+import WalletConnectButton from '@components/WalletConnectButton.js';
+import SignatureModal from '@components/SignatureModal.js';
 import signPermit from '@utils/signPermit.js'; // 导入 signPermit
 import { getWeb3Provider } from '@utils/web3Utils.js'; // 导入工具函数
-import './App.css';
+import 'components/App.css';
 
 const App = () => {
   const [signer, setSigner] = useState(null);
